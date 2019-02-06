bf8045a2d9ac797bfb892b756ec2088c02b501f6
vova <x500@ukr.net>
2019-02-06 21:00:40 +0200

    5

--- php_junior/public/models/ItemModel.php
+++ php_junior/public/models/ItemModel.php
@@ -6,5 +6,65 @@ class ItemModel extends Model {
 
     protected $table = 'items';
 
+    /*
+    $filter = ['cat' => 55, 'priceM' => 100, 'ids' => [1,2,3,4]];
+    $fields = ['name', 'price'];
+    $fields = 'count';
+    */
+
+    public function listItems($filter = [], $fields = null)
+    {
+        if(!$fields) {
+            $fields = ['name', 'price'];
+            //$fields = ['*'];
+        }
+
+        $sql = " FROM $this->table ";
+
+        if (!empty($filter)) {
+            $sql = $sql. ' WHERE ';
+            if (key_exists('çat', $filter)) {
+                $sql .= 'category_id = ?';
+            }
+            if (key_exists('priceM', $filter)) {
+                $sql .= 'price < ?';
+            }
+            if (key_exists('ids', $filter)) {
+                $sql .= 'id IN ?';
+                $filter['ids'] = "(". join(",", $filter['ids']) .")";
+            }
+        }
+
+        if ($fields) {
+            if ($fields == 'count') {
+                $sql = 'SELECT COUNT(*) '.$sql;
+            } else {
+                $sql = 'SELECT ' .join(",", $fields) .$sql;
+            }
+        }
+
+
+
+
+        var_dump($sql);
+
+        $stmt = $this->connect->prepare($sql);
+
+        if (!empty($filter)) {
+            $array_of_values = [];
+            $types = '';
+            foreach ($filter as $key => $fl) {
+                $array_of_values[] = $fl;
+                $types .= (in_array($key, ['id', "price])) ? 'i' : "s";
+            }
+            $mysqli_stmt->bind_param($types, ...$array_of_values);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $data = $result->fetch_assoc();
+
+        return $data;
+    }
+
 
 }
--- php_junior/public/models/Model.php
+++ php_junior/public/models/Model.php
@@ -16,4 +16,22 @@ class Model {
         $result = $stmt->execute();
         return $result;
     }
+
+    public function byUser($id) {
+        return $this->delete($id,
+            "SELECT c.*, g.name FROM cart AS c JOIN goods AS g ON c.product_id = g.id WHERE c.user_id = ?");
+    }
+
+    public function join($id, $sql)
+    {
+        $stmt = $this->connect->prepare($sql);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+        return $data;
+    }
+
+
+
 }
