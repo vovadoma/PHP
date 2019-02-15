c82fdd667e34e06f100214eac60e04341d622f8e
vova <x500@ukr.net>
2019-02-15 21:26:19 +0200

    5

--- php_junior/public/index.php
+++ php_junior/public/index.php
@@ -1,46 +1,43 @@
+<script src="/assets/js/jquery-3.3.1.min.js"></script>
+
+<div id="container">
+
+    <form method="post" action="">
+        <div>
+            Select:
+        </div>
+        <input type="number" name="price">
+        <br>
+        <a href="">Cat 1</a>
+        <a href="">Cat 2</a>
+        <a href="">Cat 3</a>
+        <a href="">Cat 4</a>
+        <br>
+        <a href="">Brand 33</a>
+        <a href="">Brand 34</a>
+        <br>
+        <button type="submit">Submit</button>
+        <button class="c-form-button_delete j-button_delete" type="submit">Delete</button>
+    </form>
+</div>
+
+<script>
+
+    $("document").ready( function(e) {
+
+        $('#container').on('click', '.j-button_delete', function () {
+            alert(444);
+            e.stopPropagation();
+        });
+
+
+    });
+
+</script>
 
 <?php
-
-function filter_url($param = null, $id = null) {
-    $urls = [];
-    $gets = $_GET;
-    if ($param) {
-        $gets[$param] = $id;
-    }
-    foreach ($gets as $key => $val) {
-        $urls[] = $key.'='.$val;
-    }
-    $url = 'http://php.test/?'.join('&', $urls);
-    return $url;
-}
-
-
-if (!empty($_POST)) {
-
-    $redirect_url = filter_url('price', $_POST['price']);
-    header("Location:" . "$redirect_url" );
-
-    //var_dump($_GET);
-    var_dump($redirect_url);
-    exit;
-}
-
-
 
 
 ?>
 
-<form method="post" action=" <?= filter_url() ?>">
-    <input type="number" name="price">
-    <br>
-    <a href="<?= filter_url('cat', 1) ?>">Cat 1</a>
-    <a href="<?= filter_url('cat', 2) ?>">Cat 2</a>
-    <a href="<?= filter_url('cat', 3) ?>">Cat 3</a>
-    <a href="<?= filter_url('cat', 4) ?>">Cat 4</a>
-    <br>
-    <a href="<?= filter_url('brand', 33) ?>">Brand 33</a>
-    <a href="<?= filter_url('brand', 34) ?>">Brand 34</a>
-    <br>
-    <button type="submit">Submit</button>
-</form>
 
