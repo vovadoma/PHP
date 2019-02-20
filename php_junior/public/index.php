a0645ecece7584c7d71157ba8cce70faba7e673e
lion <lion@now-here.com>
2019-02-20 21:03:30 +0200

    js

--- php_junior/public/index.php
+++ php_junior/public/index.php
@@ -1,3 +1,18 @@
+<?php
+    if (!empty($_POST)) {
+        header('Content-Type: application/json');
+
+        $deleted_id = $_POST['id'];
+        // delete from DB
+         echo json_encode([
+            'action' => true, 'deleted' => $deleted_id
+        ]);
+        exit;
+    }
+
+?>
+
+
 <script src="/assets/js/jquery-3.3.1.min.js"></script>
 
 <div id="container">
@@ -13,21 +28,37 @@
         <a href="">Cat 3</a>
         <a href="">Cat 4</a>
         <br>
-        <a href="">Brand 33</a>
-        <a href="">Brand 34</a>
-        <br>
+        <p class="j-par-33">
+            <a href="">Brand 33</a> <button data-brand-id="33" class="c-form-button_delete j-button_delete">Delete</button>
+        </p>
+        <p class="j-par-34">
+            <a href="">Brand 34</a> <button data-brand-id="34" class="c-form-button_delete j-button_delete">Delete</button><br>
+        </p>
         <button type="submit">Submit</button>
-        <button class="c-form-button_delete j-button_delete" type="submit">Delete</button>
-    </form>
+     </form>
 </div>
 
 <script>
 
     $("document").ready( function(e) {
 
-        $('#container').on('click', '.j-button_delete', function () {
-            alert(444);
-            e.stopPropagation();
+        $('#container').on('click', '.j-button_delete', function (e) {
+            var button = $(this);
+            var id = button.attr('data-brand-id');
+            $.ajax({
+                type: "POST",
+                url: "/item/delete?id=" + id,
+                data: {id: id},
+                success: function (data) {
+                    if(data.action) {
+                        var id = data.deleted;
+                        var par = $('#container').find('.j-par-' + id);
+                        par.detach();
+                        console.log(par);
+                    }
+                }
+            });
+            return false;
         });
 
 
